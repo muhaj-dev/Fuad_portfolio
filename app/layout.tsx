38f71679e8744f4f6c7e989ff401c6acf2ad9a0a
muhammod1 <muhammodholawale@gmail.com>
2023-06-25 07:39:20 +0100

    Profile component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
+import Head from "next/head";
 import { Side } from "./Components/Nav/SideNav";
 import "./globals.css";
 import { Iceland, Inter } from "next/font/google";
 import { Iceberg } from "next/font/google";
-
+import sect from '../public/img/sect.svg'
 const inter = Inter({ subsets: ["latin"] });
-const iceberg = Iceland({ 
-  subsets: ['latin'],
-  weight:  "400",
- })
+const iceberg = Iceland({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata = {
   title: "Crypto Lead",
@@ -21,17 +22,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* <Head>
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </Head> */}
       <body className={inter.className}>
         <div className="w-[100vw] bg-[#0B0E21] h-fit">
-        <div className="flex flex-row w-[100vw] max-w-[1600px] mx-auto justify-between py-[1%] px-[0.5%]">
-          <div className="fixed w-[3%] h-[94vh] max-h-[890px] py-5 bg-primary rounded-[10px]">
-            <Side />
+          <div className="flex flex-row w-[100vw] max-w-[1600px] mx-auto justify-between py-[1%] px-[0.5%]">
+            <div className="fixed w-[3%] h-[94vh] max-h-[890px] py-5 bg-primary rounded-[10px]">
+              <Side />
+            </div>
+            <div className="w-[3%] " />
+            <div className="w-[96%] ">{children}</div>
           </div>
-          <div className="w-[3%] "/>
-          <div className="w-[96%] ">
-            {children}
-          </div>
-        </div>
         </div>
       </body>
     </html>
