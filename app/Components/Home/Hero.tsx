189eed45ab2deca6b4decdebef0ede3972394426
muhammod1 <muhammodholawale@gmail.com>
2023-06-29 17:34:06 +0100

    Home page

--- app/Components/Home/Hero.tsx
+++ app/Components/Home/Hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import React, { useContext } from "react";
 import { MdKeyboardArrowRight } from "react-icons/md";
--- app/Components/Home/Job.tsx
+++ app/Components/Home/Job.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useRef } from "react";
 import Image from "next/image";
 import fire from "../../../public/img/fire.svg";
