91a18bc905c1e04e1afd4a5ba980bced4ab0ec9f
muhammod1 <muhammodholawale@gmail.com>
2023-07-01 21:34:44 +0100

    Home page for mobile

--- app/Components/Home/Nft.tsx
+++ app/Components/Home/Nft.tsx
@@ -29,7 +29,7 @@ export const Nft = () => {
         <p className="text-[#068C95] text-[24px] font-[600]">My NTFs</p>
       </div>
 
-      <ul ref={ref} className="">
+      <ul ref={ref} className="hidden tablet:flex">
         <li>
           <div className="flex gap-2 basis-fit items-center">
             <div className="rounded-full">
@@ -172,132 +172,134 @@ export const Nft = () => {
           </div>
         </li>
       </ul>
-      {/* <div className=" w-full mx-auto flex gap-16 overflow-x-scroll ">
-        <div className="flex gap-2 basis-fit items-center">
-          <Image
-            src={nftpol}
-            className=" "
-            alt="lang"
-            width={55}
-            height={55}
-            priority
-          />
-          <div className="flex gap-1 items-start">
-            <div className="">
-              <p className="text-[14px]">polypepe</p>
-              <p className="text-light_blue text-[12px] mt-2">20 MATIC</p>
-            </div>
-            <Image
-              src={redtick}
-              className="mt-1"
-              alt="lang"
-              width={15}
-              height={15}
-              priority
-            />
-          </div>
-        </div>
-        <div className="flex gap-2 basis-fit items-center">
-          <Image
-            src={nftcat1}
-            className=" "
-            alt="lang"
-            width={55}
-            height={55}
-            priority
-          />
-          <div className="flex gap-1 items-start">
-            <div className="">
-              <p className="text-[14px]">AiCat</p>
-              <p className="text-light_blue text-[12px] mt-2">---</p>
-            </div>
-            <Image
-              src={redtick}
-              className="mt-1"
-              alt="lang"
-              width={15}
-              height={15}
-              priority
-            />
-          </div>
-        </div>
-        <div className="flex gap-2 basis-[500px] items-center">
-          <Image
-            src={nftcat2}
-            className=" "
-            alt="lang"
-            width={55}
-            height={55}
-            priority
-          />
-          <div className="flex gap-1 items-start">
-            <div className="">
-              <p className="text-[14px]">AiCat</p>
-              <p className="text-light_blue text-[12px] mt-2">---</p>
-            </div>
-            <Image
-              src={redtick}
-              className="mt-1"
-              alt="lang"
-              width={15}
-              height={15}
-              priority
-            />
-          </div>
-        </div>
-        <div className="flex gap-2 basis-[500px] items-center">
-          <Image
-            src={nftzero}
-            className=" "
-            alt="lang"
-            width={55}
-            height={55}
-            priority
-          />
-          <div className="flex gap-1 items-start">
-            <div className="">
-              <p className="text-[14px]">Zeroloss</p>
-              <p className="text-light_blue text-[12px] mt-2">0.1 BNB</p>
-            </div>
-            <Image
-              src={redtick}
-              className="mt-1"
-              alt="lang"
-              width={15}
-              height={15}
-              priority
-            />
-          </div>
-        </div>
-        <div className="flex gap-2 basis-[500px] items-center">
-          <Image
-            src={nftunst}
-            className=" "
-            alt="lang"
-            width={55}
-            height={55}
-            priority
-          />
-          <div className="flex gap-1 items-start">
-            <div className="">
-              <p className="text-[14px]">Unstopa...</p>
-              <p className="text-light_blue text-[12px] mt-1">1 MATIC</p>
-            </div>
-            <Image
-              src={redtick}
-              className="mt-1"
-              alt="lang"
-              width={15}
-              height={15}
-              priority
-            />
-          </div>
-        </div>
-      </div> */}
 
-      {/* <div className="border-[1px] border-light_blue h-[8px] rounded-md mt-5">
-        <div className="w-[35%] h-full bg-light_blue" />
-      </div> */}
+      <ul ref={ref} className=" tablet_l:hidden">
+        <li>
+          <div className="relative flex flex-col items-center basis-fit">
+            <div className="rounded-full">
+              <Image
+                src={nftpol}
+                className="rounded-full tablet_l:rounded-md "
+                alt="lang"
+                width={50}
+                height={50}
+                priority
+              />
+            <div className="absolute top-0 -right-1">
+              <Image
+                src={redtick}
+                className="mt-1"
+                alt="lang"
+                width={20}
+                height={20}
+                priority
+              />
+            </div>
+                <p className="mt-1 text-[0.75rem]">polypepe</p>
+            </div>
+          </div>
+        </li>
+        <li>
+        <div className="relative flex flex-col items-center basis-fit">
+            <div className="rounded-full">
+              <Image
+                src={nftcat1}
+                className="rounded-full tablet_l:rounded-md "
+                alt="lang"
+                width={50}
+                height={50}
+                priority
+              />
+            {/* <div className="absolute top-0 -right-1">
+              <Image
+                src={redtick}
+                className="mt-1"
+                alt="lang"
+                width={20}
+                height={20}
+                priority
+              />
+            </div> */}
+                <p className="mt-1 text-[0.75rem]">AiCat</p>
+            </div>
+          </div>
+        </li>
+        <li>
+        <div className="relative flex flex-col items-center basis-fit">
+            <div className="rounded-full">
+              <Image
+                src={nftcat2}
+                className="rounded-full tablet_l:rounded-md "
+                alt="lang"
+                width={50}
+                height={50}
+                priority
+              />
+            {/* <div className="absolute top-0 -right-1">
+              <Image
+                src={redtick}
+                className="mt-1"
+                alt="lang"
+                width={20}
+                height={20}
+                priority
+              />
+            </div> */}
+                <p className="mt-1 text-[0.75rem]">AiCat</p>
+            </div>
+          </div>
+        </li>
+        <li>
+        <div className="relative flex flex-col items-center basis-fit">
+            <div className="rounded-full">
+              <Image
+                src={nftzero}
+                className="rounded-full tablet_l:rounded-md "
+                alt="lang"
+                width={50}
+                height={50}
+                priority
+              />
+            {/* <div className="absolute top-0 -right-1">
+              <Image
+                src={redtick}
+                className="mt-1"
+                alt="lang"
+                width={20}
+                height={20}
+                priority
+              />
+            </div> */}
+                <p className="mt-1 text-[0.75rem]">Zeroloss</p>
+            </div>
+          </div>
+        </li>
+        <li>
+        <div className="relative flex flex-col items-center basis-fit">
+            <div className="rounded-full">
+              <Image
+                src={nftunst}
+                className="rounded-full tablet_l:rounded-md "
+                alt="lang"
+                width={50}
+                height={50}
+                priority
+              />
+            {/* <div className="absolute top-0 -right-1">
+              <Image
+                src={redtick}
+                className="mt-1"
+                alt="lang"
+                width={20}
+                height={20}
+                priority
+              />
+            </div> */}
+                <p className="mt-1 text-[0.75rem]">Unstopa...</p>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
