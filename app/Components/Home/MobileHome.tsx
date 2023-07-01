09037ba7cb14f9affb99008d334f4d92f27a3a50
muhammod1 <muhammodholawale@gmail.com>
2023-07-01 17:15:33 +0100

    Home page for mobile

--- app/Components/Home/MobileHome.tsx
+++ app/Components/Home/MobileHome.tsx
@@ -40,7 +40,7 @@ export const MobileHome = () => {
           </div>
         </div>
         <div className="h-[100dvh] w-[100dvw] absolute b_gradient opacity-50" />
-        <div className="relative bg-transparent flex flex-col pt-3 w-full h-[30vh]">
+        <div className="relative bg-transparent flex flex-col pt-3 w-full h-[29.5vh]">
           <div className="w-[95%] mx-auto pt-3">
             <div className="flex justify-between mx-auto relative">
               <Image
